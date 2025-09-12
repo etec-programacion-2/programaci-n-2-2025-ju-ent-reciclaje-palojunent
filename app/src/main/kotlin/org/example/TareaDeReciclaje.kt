cbc4102b640588b3b173edd5dd606ebf7d47060a
palojunent <p.junet@alumno.etec.um.edu.ar>
2025-09-12 10:41:11 -0300

    issue 5, sin terminar

---
+++ app/src/main/kotlin/org/example/TareaDeReciclaje.kt
@@ -0,0 +1,12 @@
+package org.example 
+
+class TareaDeReciclaje(
+    val items: Mutablelist <ItemReciclado>
+){
+    fun agregarItem(item: ItemReciclado){
+
+    }
+    fun calcularBeneficioTotal(){
+
+    }
+}
