d9a09894c76068d8818f174ae050b064dca57cb7
palojunent <p.junet@alumno.etec.um.edu.ar>
2025-09-23 12:49:31 -0300

    issue 5, sin terminar

--- app/src/main/kotlin/org/example/TareaDeReciclaje.kt
+++ app/src/main/kotlin/org/example/TareaDeReciclaje.kt
@@ -1,12 +1,19 @@
 package org.example 
 
 class TareaDeReciclaje(
-    val items: Mutablelist <ItemReciclado>
+    val items: Mutablelist <ItemReciclado> = mutableListOf()
 ){
     fun agregarItem(item: ItemReciclado){
+            items.add(item)
+    }
+    fun calcularBeneficioTotal(): Double {
+        var BeneficioTotal= 0.0 
+        for (item in items) {
+            BeneficioTotal += item.calcularBeneficio()
 
     }
-    fun calcularBeneficioTotal(){
+            return BeneficioTotal
+
 
     }
 }
